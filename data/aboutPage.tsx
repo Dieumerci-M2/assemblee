72ba7de8d55c919c1b9c99749a6d3c7da856d3e1
MUHINDO Dieumerci <dieumerci.md09@gmail.com>
2024-05-11 13:59:24 +0200

    add about page images and index it

---
+++ data/aboutPage.tsx
@@ -0,0 +1,3 @@
+import { aboutImage } from "@/public/assets/images/aboutUs/assosiationAssemblee";
+import { bible } from "@/public/assets/images/aboutUs/assosiationAssemblee";
+import { bread } from "@/public/assets/images/aboutUs/assosiationAssemblee";
---
+++ public/assets/images/aboutUs/assosiationAssemblee/index.ts
@@ -0,0 +1,5 @@
+import aboutImage from "./aboutImage.jpg"
+import bible from "./Bible.jpg"
+import bread from "./bread.jpg"
+
+export {aboutImage, bible, bread}
